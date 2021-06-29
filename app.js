4764930e7f053a8c1387dcf7e04f348fa56733aa
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-30 00:34:38 +0200

    studies

--- app.js
+++ app.js
@@ -58,3 +58,91 @@
 
 // console.log(x*y);
 
+//**********************************************
+
+// JS Data Types
+
+// console.log ("1. " + typeof 0);
+// console.log ("2. " + typeof (3,14));
+// console.log ("3. " + typeof "hello");
+// console.log ("4. " + typeof (7+8));
+// console.log ("5. " + typeof "Oliver");
+// console.log ("6. " + typeof "");
+// console.log ("7. " + typeof true);
+// console.log ("8. " + typeof (3>=2));
+// var a;    // Value is undefined.
+// console.log ("9. " + typeof a);
+
+// var a = 111e3;
+
+// > const x = 2n ** 53n;
+// // 9007199254740992n
+// > const y = x + 1n; 
+// // 9007199254740993n
+
+// // Here are two symbols with the same description:
+// let Sym1 = Symbol("Sym")
+// let Sym2 = Symbol("Sym")
+// console.log(Sym1 === Sym2) // returns "false"
+
+// var text1 = "I'm John";
+// var text2 = 'She said, "Go ahead"';
+
+// var text = "He said, \"I am a new programmer.\"";
+//                  // output : He said, "I am a new programmer."
+
+// document.getElementById("demo").innerHTML =  // Break a code line after 
+// "Hello Dolly!";                              // an operator or comma
+
+
+// Boolean (3 > 2);     //return true
+// Boolean (2 < 3);     //return true
+// Boolean (2 > 3);     //return false 
+// Boolean (2 == 3);    //return false 
+
+
+// Boolean("Hello World");
+// Boolean(5);
+// Boolean(-5);
+// Boolean("false");
+// Boolean(3.14 + 8);   //All of these values return true
+
+// Boolean("");
+// Boolean(0);
+// Boolean(-0);
+// Boolean(null);
+// var x;
+// Boolean(x);   //All of these values return false
+
+// Object properties
+
+// var myCar = new Object();
+// myCar.make = 'Ford';
+// myCar.model = 'Mustang';
+// myCar.year = 1969;
+
+// var myCar = {
+//     make: 'Ford',
+//     model: 'Mustang',
+//     year: 1969
+// };
+
+// myCar['make'] = 'Ford';
+// myCar.model = "Mustang";
+
+
+// var length = 40; // ?
+
+// var lastName = "Clarusway"; // ?
+
+// var x = { firstName: "John", lastName: "Clarusway" }; // ?
+
+
+// console.log(typeof length);
+
+// console.log(typeof lastName);
+
+// console.log(typeof x);
+
+
+
