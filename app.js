4d8dd0d2339f2e437398d941ec8922bccdb1abc5
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-29 19:35:03 +0200

    js_studies

---
+++ app.js
@@ -0,0 +1,60 @@
+// JavaScript Variables
+
+// ******************
+
+// var myNumber =3;
+// document.write(myNumber);
+
+// JavaScript is case sensitive. This means that the variables myNumber, mynumber or MYNUMBER are not same variables. All of them are different variables.
+
+// Assignment always goes from right to left.
+
+// var x=5;
+// var y=7;
+// y=x;
+
+// In this example;
+// x=5
+// y=5
+
+//**********************
+
+// Naming Rules
+
+// Names can composed of letters, digits, underscores, and dollar signs.
+// ➤ Numbers are not allowed as the first character.
+// ➤ The first character must be;
+//    ●a letter
+//    ●an underscore ( _ )
+//    ●a dollar sign ($)
+
+// ➤ JavaScript names must not contain spaces, mathematical or logical operators.
+
+//******************************
+
+// Variables
+
+// JavaScript let and const
+
+// var  a = 10;
+// {  
+//   let b = 3;
+// }
+// console.log ("a = " + a);
+// console.log ("b = " + b);   //generates an error
+
+// const x = 5;
+// x = 7;  //generates an error
+
+// var firstName = "John";
+// var lastName = "Clarusway";
+// var age = 40;
+
+// console.log(firstName, lastName, age);
+
+// var x = 3;
+
+// var y = 10;
+
+// console.log(x*y);
+
